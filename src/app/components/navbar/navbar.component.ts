251c9dabac3c57f2d9129aba9cb556c92e2124c1
Jesus Aranda <aranda.jm@icloud.com>
2020-04-16 21:13:42 -0300

    Configuring and using auth service in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  // auth is public to use it from  the html
+  constructor(public auth: AuthService) {}
 
   ngOnInit(): void {}
 }
